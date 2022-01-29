dfd81f1e4a4c52a1604c4ce8a57696fb66c9793e
Hemant <ksingh1617@gmail.com>
2022-01-29 23:16:14 +0530

    Generate random cats by clicking the generate cat button
    This project is made by using html , css and javascript

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function generateCat() {
+    var catDiv = document.createElement('div');
+    catDiv.innerHTML = "<img class='cat-image' src='http://thecatapi.com/api/images/get?format=src&type=gif&size=small'>"
+    document.getElementById('catGeneratorDiv').appendChild(catDiv);
+}
